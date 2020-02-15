bab8fac1886c4649f02a34b2a627445630ce7a29
Рунов Денис Сергеевич <>
2020-02-15 13:31:17 +0300

    из в GetHash

--- common/File.go
+++ common/File.go
@@ -3,16 +3,16 @@ package common
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"io"
 	"log"
 )
 
 //GetHash Получить md5 хеш .
-func GetHash(file io.Reader) string {
+func GetHash(file []byte) string {
 	// var g io.Writer
 	var hasher = sha256.New()
 	// repeatStream := io.TeeReader(file, hasher)
-	_, err := io.Copy(hasher, file)
+	// _, err := io.Copy(hasher, file)
+	_, err := hasher.Write(file)
 	if err != nil {
 		log.Printf("Не удалось расчитать hash - %v", err)
 	}
